0e5ba2c259a93a2dcb30f1338016ff6c0266491c
Toni Penya-Alba <toni.penya.alba@gmail.com>
2014-07-11 19:32:01 +0200

    moved city color to top

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ BEEN_COLOR =  "#03a9f4";
 LIVED_COLOR = "#ffab40";
 SEA_COLOR =   "#b3e5fc";
 NON_VISITED_COLOR = "#e1f5fe"; 
+VISITED_CITY_COLOR = "#fff";
+
 var citySVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
 
 var map;
@@ -12,11 +14,11 @@ AmCharts.ready(function() {
                         buttonFillColor:     BEEN_COLOR,
                         buttonRollOverColor: BEEN_COLOR,
                         top:                 "300"};
-    map.imagesSettings = { rollOverColor: "#fff",
+    map.imagesSettings = { rollOverColor: VISITED_CITY_COLOR,
                            rollOverScale: 3,
                            selectedScale: 3,
-                           selectedColor: "#fff", 
-                           color: "#fff"};
+                           selectedColor: VISITED_CITY_COLOR,
+                           color: VISITED_CITY_COLOR};
 
     var dataProvider = {
         mapVar: AmCharts.maps.worldHigh,
